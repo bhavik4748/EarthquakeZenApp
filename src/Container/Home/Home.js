67e1ffbff04bc0037440d5d8cfb147d437c94da5
Bhavik Gosrani <bhavik4748@gmail.com>
2021-03-27 17:22:43 -0400

    Added Grid for Show Quake Info

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import classes from './Home.module.css';
 
- const Home = () =>{
+const Home = ({ quakeData }) => {
+    let dataRow = '';
+    dataRow = quakeData.features.map(row => {
+        return (
+            <div key={row.properties.title} className={classes.tableClassRow}>
+                <div className={classes.titleRow}>{row.properties.place}</div>
+                <div className={classes.magnitudeRow}>{row.properties.mag}</div>
+                <div className={classes.timeRow}>{row.properties.formattedTime}</div>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            Home Page
+        <div className={classes.homeClass}>
+            <div className={classes.textCenter} >{quakeData.metadata.title} </div>
+
+            <div className={classes.tableClass}>
+                <div className={classes.titleHeader}>Title</div>
+                <div className={classes.magnitudeHeader}>Magnitude</div>
+                <div className={classes.timeHeader}>Time</div>
+            </div>
+
+            {dataRow}
         </div>
     )
 
--- src/services/service.js
+++ src/services/service.js
@@ -8,7 +8,22 @@ service.headers = {
 service.getApiData = async () => {
     const res = await fetch('api.json', service.headers);
     const result = await res.json();
+    let { data } = result;
+    for (let obj of data.features) {
+        obj['properties']['formattedTime'] = service.getDateTimeFromTime(obj['properties']['time']);
+    }
     return result;
+}
+
+service.monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+service.getDateTimeFromTime = (time) => {
+    const fullDate = new Date(time);
+    return `${service.monthNames[fullDate.getMonth()]}
+    ${fullDate.getUTCDay()},
+    ${fullDate.getFullYear()}
+    ${(fullDate.getHours() % 12).toString().padStart(2, 0)}:${fullDate.getMinutes().toString().padStart(2, 0)}
+    ${(fullDate.getHours() / 12) >= 1 ? 'PM' : 'AM'}`;
 }
 
 export default service;
